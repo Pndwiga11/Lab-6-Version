fb17fb7ff6153756a4b98b9841287c96eea2a3b1
Pndwiga11 <149002902+Pndwiga11@users.noreply.github.com>
2023-10-25 13:52:54 -0400

    Add files via upload

---
+++ Version6.py
@@ -0,0 +1,45 @@
+def encode(password):
+    encoded = ''
+    for i in range(0,len(password)):
+        digit = int(password[i]) + 3
+        if digit > 9:
+            digit = digit % 10
+        encoded += str(digit)
+    return encoded
+
+def decode(password):
+    decoded = ''
+    for i in range(0, len(password)):
+        digit = int(password[i]) - 3
+        if digit < 0:
+            digit = digit + 10
+        decoded += str(digit)
+    return decoded
+
+print(encode('9999'))
+print(decode(encode('0709')))
+
+def main():
+
+    encoded = None
+
+    while True:
+        print('Menu')
+        print('------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print()
+
+        menu_selection = int(input('Please enter an option: '))
+        if menu_selection == 1:
+            password = input('Please enter your password to encode: ')
+            encoded = encode(password)
+        elif menu_selection == 2:
+            decoded = decoded(encoded)
+            print('The enncoded password is ' + encoded + ' and the original passoword is ' + decoded + '.')
+        elif menu_selection == 3:
+            break
+
+if __name__ == "__main__":
+    main()
